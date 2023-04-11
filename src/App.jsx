3185e07d310764b281ab1dcf1745f51d6e024ccb
ogzCode <oguzguc44@gmail.com>
2023-04-11 10:50:31 +0300

    Styles edited and components added to main component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+     <Home/>
+     <About/>
+    </div>
+  )
+}
+
+export default App
