a709669cacc7e66e887bfb56bbaa7158c746a427
ogzCode <oguzguc44@gmail.com>
2023-04-12 11:34:52 +0300

    Added react-router for Project and Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css';
 import Header from './components/Header';
-import Home from './components/Home';
-import About from './components/About';
+import Contact from './components/Contact';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
      <Header/>
-     <Home/>
-     <About/>
+     <Outlet/>
+     <Contact/>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from "./components/main/Main";
+import Projects from './components/project/Projects';
+import Design from './components/desing/Design';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path: "/",
+        element: <Main/>
+      },
+      {
+        path: "/project",
+        element: <Projects/>
+      },
+      {
+        path: "/design",
+        element: <Design/>
+      }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router}/>
 )
