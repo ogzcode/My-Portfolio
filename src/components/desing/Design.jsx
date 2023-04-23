23427fbf4936dd88cd3504c0a770c09018de707f
ogzCode <oguzguc44@gmail.com>
2023-04-23 14:36:46 +0300

    Day 17 Design added

--- src/components/desing/Design.jsx
+++ src/components/desing/Design.jsx
@@ -16,6 +16,7 @@ import day13 from "./img/day13.svg";
 import day14 from "./img/day14.svg";
 import day15 from "./img/day15.svg";
 import day16 from "./img/day16.svg";
+import day17 from "./img/day17.svg";
 
 function Card({ count, img }) {
     return (
@@ -28,7 +29,7 @@ function Card({ count, img }) {
 
 function Design() {
     const data = [day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11,
-        day12, day13, day14, day15, day16
+        day12, day13, day14, day15, day16, day17
     ];
     return (
         <div className="min-h-screen py-6">
