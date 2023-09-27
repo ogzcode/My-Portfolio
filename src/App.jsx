a5f5d1c5963883dbf596b004fc25b45dad062d58
ogzCode <oguzguc44@gmail.com>
2023-09-27 16:35:10 +0300

    Bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import NavBar from "./components/navBar";
+import NavBar from "./components/NavBar";
 import Tools from "./components/Tools";
 import Timeline from "./components/Timeline";
 import Contact from "./components/Contact";
