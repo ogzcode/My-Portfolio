1bbc73f8ac789e66f08e11bf8fedea4b9efeacfa
ogzCode <oguzguc44@gmail.com>
2024-07-19 10:01:20 +0300

    chore: Remove unused files and configurations

--- src/components/Divider.jsx
+++
@@ -1,7 +0,0 @@
-export const Divider = () => {
-    return (
-        <div className="text-center mt-16">
-            <span className="inline-block w-1/3 bg-stone-300 h-px"></span>
-        </div>
-    );
-}
