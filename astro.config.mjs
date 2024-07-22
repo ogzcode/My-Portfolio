1b524daec9ae9ffe7a0f60ba8b10579aca24138f
ogzCode <oguzguc44@gmail.com>
2024-07-22 15:43:08 +0300

    chore: Update base URL in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import icon from 'astro-icon'
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://github.com/ogzcode',
-	base: "/",
+	base: "/oguzhanGuc",
 	integrations: [
 		expressiveCode(expressiveCodeOptions),
 		tailwind({
